d4d5bdf785349a82db6fe7f4c8d8462aeb06243d
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-07-13 17:21:59 -0400

    Update hero section styles

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,7 @@ import Container from "@/components/container"
 
 export default function Hero() {
   return (
-    <Container className="flex flex-col md:flex-row items-center justify-evenly md:justify-between min-h-dvh py-10">
+    <Container className="flex flex-col md:flex-row items-center justify-evenly md:justify-between min-h-dvh py-10 gap-y-10">
       <div className="flex items-center max-w-sm lg:max-w-lg">
         <div className="flex flex-col gap-y-5">
           <h1 className="font-bold text-4xl md:text-5xl">
